10d1438f29198a90716d8198dad8cd8cf3d97fbf
Eric Lemonne <eric@c50.be>
2014-01-07 18:05:20 +0100

    deactivate copyright

--- templates/region--footer-first.tpl.php
+++ templates/region--footer-first.tpl.php
@@ -1,7 +1,7 @@
 <div<?php print $attributes; ?>>
   <div<?php print $content_attributes; ?>>
  	<div id="patte"><a class="go-top" href="#top" title="top">top</a></div>
-<a href="/"><img src="/<?php print path_to_theme(); ?>/images/logogroovykitty.png" width="100px" alt="Groovy Kitty"/></a>
-	  <?php print $content; ?>	<p id="copy">Copyright Elvire Cavali&eacute; &copy; 2012</p>
+<!-- <a href="/"><img src="/<?php print path_to_theme(); ?>/images/logogroovykitty.png" width="100px" alt="Groovy Kitty"/></a> -->
+	  <?php print $content; ?>	<p id="copy">Copyright LoraineV &copy; 2014</p>
 	</div>
 </div>
