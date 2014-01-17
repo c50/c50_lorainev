420cdd3896c180e3c96e37906576153c97de515e
Eric Lemonne <eric@c50.be>
2014-01-17 14:43:36 +0100

    Add city

--- templates/region--footer-first.tpl.php
+++ templates/region--footer-first.tpl.php
@@ -2,6 +2,6 @@
   <div<?php print $content_attributes; ?>>
  	<div id="patte"><a class="go-top" href="#top" title="top">top</a></div>
 	 <a href="/"><img src="/<?php print path_to_theme(); ?>/images/lorainev_beige.png" width="100px" alt="Loraine V"/></a>
-	  <?php print $content; ?>	<p id="copy">Copyright LoraineV &copy; 2014</p>
+	  <?php print $content; ?>	<p id="copy">Copyright LoraineV, Bruxelles &copy; 2014</p>
 	</div>
 </div>
