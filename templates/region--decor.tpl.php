738d5c45b9a96a2cca7aacbcce23cb42d6484221
Eric Lemonne <eric@c50.be>
2014-01-21 20:27:47 +0100

    add a decor image to content

--- templates/region--decor.tpl.php
+++ templates/region--decor.tpl.php
@@ -1,8 +1,7 @@
 <div<?php print $attributes; ?>>
   <div<?php print $content_attributes; ?>>
-     <!-- <ul>
-	   <li><img src="/<?php print path_to_theme(); ?>/images/parlophone.png" alt="parlophone" /></li>
+      <ul>
+	   <li><img src="/<?php print path_to_theme(); ?>/images/tablier.png" alt="tablier" /></li>
 	   <li><img src="/<?php print path_to_theme(); ?>/images/griffes.png" alt="griffes" /></li>
-		</ul> -->
-	</div>
+		</ul> 	</div>
 </div>
